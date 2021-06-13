b000ac28d6ba18f3065480e3c6ffa2b5df9a2672
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-06-13 23:30:41 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/signup.html");
 });
 
+app.post("/", function (req, res) {});
+
 app.listen(3000, function () {
   console.log("Server is running on port 3000");
 });
