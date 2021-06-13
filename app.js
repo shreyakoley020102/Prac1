71e23d97264bfa12f898194b6d2c0a6d36809206
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-06-13 23:25:18 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const request = require("request");
 
 const app = express();
 
+app.use(express.static("public"));
+
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/signup.html");
 });
